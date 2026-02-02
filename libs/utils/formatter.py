7958321f67ee1da4c2472980aa2b12d50f01028d
togakushi <>
2026-02-02 14:21:25 +0900

    Refactor name_replace function for improved clarity in string conversion comments

--- libs/utils/formatter.py
+++ libs/utils/formatter.py
@@ -130,13 +130,13 @@ def name_replace(target: str, add_mark: bool = False, not_replace: bool = False)
 
     chk_pattern = [
         target,  # 無加工
-        textutil.str_conv(target, textutil.ConversionType.HtoZ),  # 半角数字 -> 全角数字
-        textutil.str_conv(target, textutil.ConversionType.KtoH),  # カタカナ -> ひらがな
-        textutil.str_conv(target, textutil.ConversionType.HtoK),  # ひらがな -> カタカナ
+        textutil.str_conv(target, textutil.ConversionType.HtoZ),  # 半角 -> 全角
+        textutil.str_conv(target, textutil.ConversionType.KtoH),  # カタ -> ひら
+        textutil.str_conv(target, textutil.ConversionType.HtoK),  # ひら -> カタ
         honor_remove(target),  # 敬称削除
-        honor_remove(textutil.str_conv(target, textutil.ConversionType.HtoZ)),
-        honor_remove(textutil.str_conv(target, textutil.ConversionType.KtoH)),
-        honor_remove(textutil.str_conv(target, textutil.ConversionType.HtoK)),
+        honor_remove(textutil.str_conv(target, textutil.ConversionType.HtoZ)),  # 敬称削除 + 半角 -> 全角
+        honor_remove(textutil.str_conv(target, textutil.ConversionType.KtoH)),  # 敬称削除 + カタ -> ひら
+        honor_remove(textutil.str_conv(target, textutil.ConversionType.HtoK)),  # 敬称削除 + カタ -> ひら
     ]
     chk_pattern = sorted(set(chk_pattern), key=chk_pattern.index)  # 順序を維持したまま重複排除
 
