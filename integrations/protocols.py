37d2488e1ae699fb7e65412ebb4d0518578abef3
togakushi <>
2026-02-07 13:09:47 +0900

    Update PostData class docstrings for clarity and consistency

--- integrations/protocols.py
+++ integrations/protocols.py
@@ -120,13 +120,10 @@ class MsgData(DataMixin):
 class PostData(DataMixin):
     """ポストするデータ"""
 
-    """本文メッセージ"""
     headline: Optional[tuple["MessageType", "StyleOptions"]] = field(default=None)
-    """ヘッダ文"""
+    """ヘッダメッセージ"""
     message: list[tuple["MessageType", "StyleOptions"]] = field(default_factory=list)
-    """本文
-    MessageTypeとStyleOptionsのペアのリスト
-    """
+    """本文メッセージ"""
     thread: bool = field(default=True)
     """スレッドに返す"""
     ts: str = field(default="undetermined")
