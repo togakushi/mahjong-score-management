22bbcd608a557bf475667e6f0749f324cf2c405d
togakushi <>
2026-03-05 16:55:38 +0900

    Update library/module documentation for clarity and organization

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,17 +1,18 @@
-"""共通ライブラリ/モジュール
+"""ライブラリ/モジュール
 
+アプリケーションライブラリ:
 - `libs.domain`: アプリケーションロジック
 - `libs.commands`: サブコマンド
 
-ライブラリ:
-- `libs.data`: データ操作ライブラリ
-- `libs.functions`: 共通処理ライブラリ
-- `libs.utils`: ユーティリティ
-
-モジュール:
+アプリケーションモジュール:
 - `libs.types`: 共通型定義
 - `libs.datamodels`: データモデル
 - `libs.configuration`: 初期設定関数
 - `libs.global_value`: モジュール間共通変数
 - `libs.dispatcher`: コマンドディスパッチャ
+
+共有ライブラリ:
+- `libs.data`: データ操作ライブラリ
+- `libs.functions`: 共通処理ライブラリ
+- `libs.utils`: ユーティリティ
 """
