5e04776757e8e41aed54c41ef1cce6f42615d323
togakushi <>
2026-03-05 15:18:29 +0900

    Update version to 0.10.1 and reorganize domain management structure

--- cls/__init__.py
+++ cls/__init__.py
@@ -3,8 +3,5 @@
 
 - `cls.config`: 設定関連
 - `cls.command`: コマンド引数解析
-- `cls.score`: スコア解析
-- `cls.stats`: 成績解析
-- `cls.rule`: ルールセット管理
 - `cls.timekit`: datetime拡張
 """
---
+++ libs/domain/__init__.py
@@ -0,0 +1,6 @@
+"""ドメイン管理
+
+- `libs.domain.score`: スコア解析
+- `libs.domain.stats`: 成績解析
+- `libs.domain.rule`: ルールセット管理
+"""
