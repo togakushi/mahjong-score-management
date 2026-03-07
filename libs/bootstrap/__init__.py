3c99c14cc96c0ad04c22e064b57a6456a8a2a596
togakushi <>
2026-03-07 20:38:08 +0900

    Update documentation in __init__.py for clarity on app_config and section handling

--- libs/bootstrap/__init__.py
+++ libs/bootstrap/__init__.py
@@ -1,7 +1,7 @@
 """
 アプリケーション起動処理
 
-- `libs.bootstrap.app_config`: 設定関連
-- `libs.bootstrap.section`: セクション設定取り込み
 - `libs.bootstrap.configuration`: 初期設定関数
+- `libs.bootstrap.app_config`: アプリケーション設定関連
+- `libs.bootstrap.section`: セクション情報取り込み
 """
