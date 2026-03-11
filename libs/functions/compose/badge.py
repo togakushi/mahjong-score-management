6ef558621409b1bc5c4b4d284d89cddfa2a3b8bd
togakushi <>
2026-03-11 15:55:01 +0900

    Refactor badge.py to remove unnecessary type checks and improve configuration access for clarity and consistency

--- libs/functions/compose/badge.py
+++ libs/functions/compose/badge.py
@@ -3,13 +3,10 @@ libs/functions/compose/badge.py
 """
 
 import math
-from typing import TYPE_CHECKING, cast
+from typing import cast
 
 import libs.global_value as g
 from libs.data import aggregate, loader
-
-if TYPE_CHECKING:
-    from configparser import ConfigParser
 
 
 def degree(game_count: int = 0) -> str:
@@ -26,11 +23,11 @@ def degree(game_count: int = 0) -> str:
     badge: str = ""
 
     if g.adapter.conf.badge_degree:
-        if degree_list := cast("ConfigParser", getattr(g.cfg, "_parser")).get("degree", "badge", fallback=""):
+        if degree_list := g.cfg.setting.main_parser.get("degree", "badge", fallback=""):
             degree_badge = degree_list.split(",")
         else:
             return ""
-        if counter_list := cast("ConfigParser", getattr(g.cfg, "_parser")).get("degree", "counter", fallback=""):
+        if counter_list := g.cfg.setting.main_parser.get("degree", "counter", fallback=""):
             degree_counter = list(map(int, counter_list.split(",")))
             for idx, val in enumerate(degree_counter):
                 if game_count >= val:
@@ -54,12 +51,12 @@ def status(game_count: int = 0, win: int = 0) -> str:
     badge: str = ""
 
     if g.adapter.conf.badge_status:
-        if status_list := cast("ConfigParser", getattr(g.cfg, "_parser")).get("status", "badge", fallback=""):
+        if status_list := g.cfg.setting.main_parser.get("status", "badge", fallback=""):
             status_badge = status_list.split(",")
         else:
             return badge
 
-        if status_step := cast("ConfigParser", getattr(g.cfg, "_parser")).getfloat("status", "step", fallback=""):
+        if status_step := g.cfg.setting.main_parser.getfloat("status", "step", fallback=""):
             if not isinstance(status_step, float):
                 return badge
             if game_count == 0:
