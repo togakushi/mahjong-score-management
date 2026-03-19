dd985c07c483fad98bab44acf84488ff43c11393
togakushi <>
2026-03-19 16:45:04 +0900

    Refactor help_message function to consistently use g.params.rule_version for command lookups and improve clarity in help messages

--- libs/commands/help/entry.py
+++ libs/commands/help/entry.py
@@ -56,31 +56,32 @@ def help_message(m: "MessageParserProtocol") -> None:
         }
     )
     g.cfg.rule.status_update(g.params.placeholder())
-
-    if not (rule_version := g.params.rule_version):
-        rule_version = g.cfg.setting.default_rule
+    g.params.update_setting(g.cfg.config_file, "default_rule", str)
 
     m.set_message(
-        "使い方：<呼び出しワード> [検索範囲] [対象メンバー] [オプション]",
+        textwrap.dedent(f"""\
+        使い方：<呼び出しワード> [検索範囲] [対象メンバー] [オプション]
+        集計対象ルール識別子：{g.params.rule_version}
+        """),
         StyleOptions(title="機能呼び出し"),
     )
     m.set_message(
         textwrap.dedent(f"""\
-        呼び出しワード：{"、".join(lookup.resolve_commands(rule_version, CommandType.RESULTS))}
+        呼び出しワード：{"、".join(lookup.resolve_commands(g.params.rule_version, CommandType.RESULTS))}
         検索範囲デフォルト：{g.cfg.results.aggregation_range}
         """),
         StyleOptions(title="成績サマリ", indent=1, sub_title=True),
     )
     m.set_message(
         textwrap.dedent(f"""\
-        呼び出しワード：{"、".join(lookup.resolve_commands(rule_version, CommandType.GRAPH))}
+        呼び出しワード：{"、".join(lookup.resolve_commands(g.params.rule_version, CommandType.GRAPH))}
         検索範囲デフォルト：{g.cfg.graph.aggregation_range}
         """),
         StyleOptions(title="成績グラフ", indent=1, sub_title=True),
     )
     m.set_message(
         textwrap.dedent(f"""\
-        呼び出しワード：{"、".join(lookup.resolve_commands(rule_version, CommandType.RANKING))}
+        呼び出しワード：{"、".join(lookup.resolve_commands(g.params.rule_version, CommandType.RANKING))}
         検索範囲デフォルト：{g.cfg.ranking.aggregation_range}
         規定打数デフォルト：全体ゲーム数 × {g.cfg.ranking.stipulated_rate} ＋ 1
         出力制限デフォルト：上位 {g.cfg.ranking.ranked} 名
@@ -89,17 +90,17 @@ def help_message(m: "MessageParserProtocol") -> None:
     )
     m.set_message(
         textwrap.dedent(f"""\
-        呼び出しワード：{"、".join(lookup.resolve_commands(rule_version, CommandType.REPORT))}
+        呼び出しワード：{"、".join(lookup.resolve_commands(g.params.rule_version, CommandType.REPORT))}
         検索範囲デフォルト：{g.cfg.report.aggregation_range}
         """),
         StyleOptions(title="レポート", indent=1, sub_title=True),
     )
     m.set_message(
-        f"呼び出しワード：{'、'.join(lookup.resolve_commands(rule_version, CommandType.MEMBER_LIST))}",
+        f"呼び出しワード：{'、'.join(lookup.resolve_commands(g.params.rule_version, CommandType.MEMBER_LIST))}",
         StyleOptions(title="メンバー一覧", indent=1, sub_title=True),
     )
     m.set_message(
-        f"呼び出しワード：{'、'.join(lookup.resolve_commands(rule_version, CommandType.TEAM_LIST))}",
+        f"呼び出しワード：{'、'.join(lookup.resolve_commands(g.params.rule_version, CommandType.TEAM_LIST))}",
         StyleOptions(title="チーム一覧", indent=1, sub_title=True),
     )
     m.set_message(  # 検索範囲
@@ -109,14 +110,14 @@ def help_message(m: "MessageParserProtocol") -> None:
 
     # メモ機能
     remarks_type1: str = ""
-    if words := lookup.regulation_list(1, rule_version):
+    if words := lookup.regulation_list(1, g.params.rule_version):
         remarks_type1 += "個別カウントワード：" + "、".join(words)
     else:
         if g.cfg.rule.get_undefined_word(g.params.default_rule) == 1:
             remarks_type1 += "個別カウントワード：未登録ワードのすべてを個別にカウント"
 
     remarks_type0: str = ""
-    if words := lookup.regulation_list(0, rule_version):
+    if words := lookup.regulation_list(0, g.params.rule_version):
         remarks_type0 += "役満カウントワード：" + "、".join(words)
     else:
         if g.cfg.rule.get_undefined_word(g.params.default_rule) == 0:
@@ -125,7 +126,7 @@ def help_message(m: "MessageParserProtocol") -> None:
     m.set_message(
         textwrap.dedent(f"""\
         使い方：<メモ記録ワード> <対象メンバー> <内容>
-        メモ記録ワード：{"、".join(g.cfg.rule.data[rule_version].remarks_words)}
+        メモ記録ワード：{"、".join(g.cfg.rule.data[g.params.rule_version].remarks_words)}
 
         {remarks_type1}
         {remarks_type0}
@@ -135,19 +136,19 @@ def help_message(m: "MessageParserProtocol") -> None:
 
     # ルールセット
     m.set_message(
-        g.cfg.rule.print(rule_version),
+        g.cfg.rule.print(g.params.rule_version),
         StyleOptions(title="ルールセット情報"),
     )
 
     # レギュレーション
     regulation: str = ""
-    if words := lookup.regulation_list(2, rule_version):
+    if words := lookup.regulation_list(2, g.params.rule_version):
         regulation += "卓外清算ワード(個人)：\n"
         for word in words:
             regulation += f"\t{word}\n"
         regulation += "\n"
 
-    if words := lookup.regulation_list(3, rule_version):
+    if words := lookup.regulation_list(3, g.params.rule_version):
         regulation += "卓外清算ワード(チーム)：\n"
         for word in words:
             regulation += f"\t{word}\n"
@@ -160,7 +161,7 @@ def help_message(m: "MessageParserProtocol") -> None:
     m.set_message(
         textwrap.dedent(f"""\
         チャンネル識別子：{g.params.source}
-        デフォルトルール：{g.params.default_rule}
+        デフォルトルール識別子：{g.params.default_rule}
         チャンネル個別設定：{channel_config.name if channel_config else "---"}
         セパレート機能：{"有効" if g.params.separate else "無効"}
         """),
