438f9779a3ad49c8986cb30f50db44249279a67f
togakushi <>
2026-03-25 01:45:56 +0900

    Fix typo in setup_rule_data function name and enhance rule configuration loading logic

--- libs/data/initialization.py
+++ libs/data/initialization.py
@@ -28,7 +28,7 @@ def main(init_db: bool) -> None:
         setup_resultdb(g.cfg.setting.database_file)  # DB初期化
 
     setup_grade_table()  # 段位テーブル取り込み
-    setup_rul_data()  # ルールデータ取り込み
+    setup_rule_data()  # ルールデータ取り込み
     setup_regulations(g.cfg.setting.database_file)  # レギュレーション設定取り込み
 
 
@@ -113,28 +113,39 @@ def setup_resultdb(database_file: Union[str, Path]) -> None:
     memdb.close()
 
 
-def setup_rul_data() -> None:
+def setup_rule_data() -> None:
     """ルールデータ取り込み"""
-    if g.cfg.setting.rule_config and not g.cfg.setting.rule_config.exists():
-        new_conf = g.cfg.config_dir / str(g.cfg.setting.rule_config)
-        if new_conf.exists():
-            g.cfg.setting.rule_config = new_conf
-        else:
-            g.cfg.setting.rule_config = None
-
     if g.cfg.main_parser.has_section("mahjong"):
         section_data = dict(g.cfg.main_parser["mahjong"])
         if rule_version := section_data.get("rule_version"):
             g.cfg.rule.data_set("mahjong", rule_data=section_data)
 
-    if not g.cfg.rule.rule_list and not g.cfg.setting.rule_config:
-        g.cfg.setting.rule_config = Path("files/default_rule.ini")
+    # ルール設定ファイル探索 & 取り込み
     if g.cfg.setting.rule_config:
+        if not g.cfg.setting.rule_config.exists():
+            if (new_conf := g.cfg.config_dir / str(g.cfg.setting.rule_config)) and new_conf.exists():
+                g.cfg.setting.rule_config = new_conf
+            elif (new_conf := g.cfg.script_dir / str(g.cfg.setting.rule_config)) and new_conf.exists():
+                g.cfg.setting.rule_config = new_conf
+            elif (new_conf := Path.cwd() / str(g.cfg.setting.rule_config)) and new_conf.exists():
+                g.cfg.setting.rule_config = new_conf
+            else:
+                g.cfg.setting.rule_config = None
+        if g.cfg.setting.rule_config:
+            g.cfg.rule.read_config(g.cfg.setting.rule_config)
+
+    # ルールセットがなければプリセットから取り込み
+    if not g.cfg.rule.rule_list:
+        if (new_conf := g.cfg.config_dir / Path("files/default_rule.ini")) and new_conf.exists():
+            g.cfg.setting.rule_config = new_conf
+        elif (new_conf := g.cfg.script_dir / Path("files/default_rule.ini")) and new_conf.exists():
+            g.cfg.setting.rule_config = new_conf
         g.cfg.rule.read_config(g.cfg.setting.rule_config)
 
     if not g.cfg.setting.default_rule:
         g.cfg.setting.default_rule = g.cfg.rule.rule_list[0]
 
+    # マッピング生成
     if g.cfg.main_parser.has_section("keyword_mapping"):
         for keyword, rule_version in dict(g.cfg.main_parser["keyword_mapping"]).items():
             if not rule_version:
@@ -150,7 +161,6 @@ def setup_rul_data() -> None:
 
     g.cfg.rule.status_update(g.params.placeholder())
     g.cfg.rule.register_to_database()
-    g.cfg.rule.info()
 
 
 def setup_regulations(database_file: Union[str, Path]) -> None:
