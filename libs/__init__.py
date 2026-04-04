1ae69a34ef0185ad09fe5eecb5ac6a3c9fa32e7e
togakushi <>
2026-04-04 19:49:47 +0900

    Update documentation in __init__.py files to improve clarity and organization

--- libs/__init__.py
+++ libs/__init__.py
@@ -2,16 +2,19 @@
 ライブラリ/モジュール
 
 アプリケーションライブラリ:
+
 - `libs.bootstrap`: アプリケーション起動処理
 - `libs.domain`: アプリケーションロジック
 - `libs.commands`: サブコマンド
 
 アプリケーションモジュール:
+
 - `libs.types`: 共通型定義
 - `libs.global_value`: モジュール間共通変数
 - `libs.dispatcher`: コマンドディスパッチャ
 
 共有ライブラリ:
+
 - `libs.functions`: 共通処理ライブラリ
 - `libs.utils`: ユーティリティ
 """
