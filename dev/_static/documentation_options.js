0c6f26b56e7f62c5277ced7ef0323f7ccf721354
togakushi <togakushi@users.noreply.github.com>
2026-04-22 08:38:58 +0000

    deploy: bed55f242ab68a702ba42e848fb4cf927bc5f0bb

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,11 +1,11 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.13.2',
+    VERSION: '0.13.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
-    HAS_SOURCE: true,
+    HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
