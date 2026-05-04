47cfce49a6aec0b612f2f17123c65d9cbafeecbd
togakushi <>
2026-05-04 13:31:37 +0900

    Refactor adjusting_values function to ensure consistent float formatting and improve handling of string values

--- libs/utils/formatter.py
+++ libs/utils/formatter.py
@@ -596,23 +596,23 @@ def adjusting_values(df: pd.DataFrame, compact: bool = False) -> pd.DataFrame:
     for column_name in df.columns:
         match column_name:
             case x if x.endswith("ポイント"):
-                df[column_name] = df[column_name].map(lambda v: f"{v:+.1f}pt".replace("-", "▲"))
+                df[column_name] = df[column_name].map(lambda v: str(v) if str(v).endswith("pt") else f"{float(v):+.1f}pt".replace("-", "▲"))
                 if compact:
                     new_name = "\n".join([x if x else "ポイント" for x in column_name.split("ポイント")])
                     df.rename(columns={column_name: new_name}, inplace=True)
             case x if x.endswith("率"):
-                df[column_name] = df[column_name].map(lambda v: f"{v:.2f}%")
+                df[column_name] = df[column_name].map(lambda v: f"{float(v):.2f}%")
             case "平均順位":
-                df[column_name] = df[column_name].map(lambda v: f"{v:.2f}")
+                df[column_name] = df[column_name].map(lambda v: f"{float(v):.2f}")
                 if compact:
                     df.rename(columns={column_name: "平均\n順位"}, inplace=True)
             case x if x == "point" or x.endswith("_point"):
-                df[column_name] = df[column_name].map(lambda v: f"{v:+.1f}pt".replace("-", "▲"))
+                df[column_name] = df[column_name].map(lambda v: str(v) if str(v).endswith("pt") else f"{float(v):+.1f}pt".replace("-", "▲"))
             case x if x == "rpoint" or x.endswith("_rpoint"):
-                df[column_name] = df[column_name].map(lambda v: f"{v:.0f}点".replace("-", "▲"))
+                df[column_name] = df[column_name].map(lambda v: f"{float(v):.0f}点".replace("-", "▲"))
             case x if x == "rank" or x.endswith("_rank"):
-                df[column_name] = df[column_name].map(lambda v: f"{v:.0f}位" if v.is_integer() else f"{v:.1f}位")
+                df[column_name] = df[column_name].map(lambda v: f"{float(v):.0f}位" if v.is_integer() else f"{v:.1f}位")
             case x if x.startswith("diff_from_"):
-                df[column_name] = df[column_name].map(lambda v: f"{v:.1f}pt" if pd.notna(v) else "------")
+                df[column_name] = df[column_name].map(lambda v: f"{float(v):.1f}pt" if pd.notna(v) else "------")
 
     return df
