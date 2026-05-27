62f6f161c83814ce67fb1e3e9a74b36cef2538b1
togakushi <>
2026-05-27 11:24:02 +0900

    Refactor Napoleon settings: disable numpy docstring support and ensure inclusion of init, private, and special methods

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -37,7 +37,11 @@ autoclass_content = "class"
 
 # -- Napoleon settings -------------------------------------------------------
 napoleon_google_docstring = True
-napoleon_numpy_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_ivar = True
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = "python_docs_theme"
