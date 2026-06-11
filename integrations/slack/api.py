8e0cbbc8bf1d098fcca2aeffe968595f593beb18
togakushi <>
2026-06-11 10:25:43 +0900

    Remove SCORE_ANALYSIS case from post_msg processing in AdapterAPI

--- integrations/slack/api.py
+++ integrations/slack/api.py
@@ -134,8 +134,6 @@ class AdapterAPI(APIInterface):
                         post_msg.extend(_table_data(converter.df_to_ranking(data, options.title, step=50)))
                     case StyleOptions.DataKind.RATING:
                         post_msg.extend(_table_data(converter.df_to_text_table(data, options, step=20)))
-                    case StyleOptions.DataKind.SCORE_ANALYSIS:
-                        post_msg.extend(_table_data(converter.df_to_text_table(data, options, step=50)))
                     case _:
                         pass
 
