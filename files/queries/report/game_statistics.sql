7b9669fb4c3373a3e80d6f6d6ccbf4ab454ce508
togakushi <>
2026-07-11 09:46:01 +0900

    fix: Update game_statistics query to correct deposit calculation and improve output formatting

--- files/queries/report/game_statistics.sql
+++ files/queries/report/game_statistics.sql
@@ -3,11 +3,11 @@ select
     --[monthly] strftime('%Y-%m', collection_daily) as 集計月,
     --[yearly] strftime('%Y', collection_daily) as 集計年,
     count() / 4 as 対戦数,
-    replace(printf('%.1fpt', round(sum(point), 1)), '-', '▲') as 供託,
+    round(sum(point), 1) + 0 as deposit,
     count(rpoint < -1 or null) as '飛んだ人数(延べ)',
-    printf('%.2f%',	round(cast(count(rpoint < -1 or null) as real) / cast(count() / 4 as real) * 100, 2)) as トビ終了率,
-    replace(printf('%s', max(rpoint)), '-', '▲') as 最大素点,
-    replace(printf('%s', min(rpoint)), '-', '▲') as 最小素点
+    round(cast(count(rpoint < -1 or null) as real) / cast(count() / 4 as real) * 100, 2) as トビ終了率,
+    max(rpoint) * 100 as rpoint_max,
+    min(rpoint) * 100 as rpoint_min
 from
     individual_results as results
 
@@ -20,8 +20,7 @@ where
     and game_info.playtime between :starttime and :endtime
     --[separate] and game_info.source = :source
     --[search_word] and game_info.comment like :search_word
-    --[individual] --[guest_not_skip] and game_info.guest_count <= 1 -- ゲストアリ(2ゲスト戦除外)
-    --[individual] --[guest_skip] and results.guest = 0 -- ゲストナシ
+    --[guest_not_skip] and game_info.guest_count <= 1 -- ゲストアリ(2ゲスト戦除外)
 group by
     --[monthly] strftime('%Y-%m', collection_daily)
     --[yearly] strftime('%Y', collection_daily)
