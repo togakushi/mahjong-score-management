66c4326f81fca6015282da878703949874c834f6
togakushi <>
2026-08-11 12:00:50 +0900

    feat: Add event processing documentation to web output

--- integrations/web/__init__.py
+++ integrations/web/__init__.py
@@ -1,6 +1,7 @@
 """
 Web出力
 
+- :doc:`integrations.web.events`: イベント処理
 - :doc:`integrations.web.adapter`: サービスアダプタ定義
 - :doc:`integrations.web.api`: インターフェースAPI
 - :doc:`integrations.web.parser`: メッセージ解析クラス
